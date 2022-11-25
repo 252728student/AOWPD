db22ea9629b788e2f69b48695583dc8af3ffa26d
252728student <115942043+252728student@users.noreply.github.com>
2022-11-25 01:00:30 +0100

    Sito Eratostenesa sekwencyjne

---
+++ SitoErastotenesaSekwencyjne.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <chrono>
+#include <thread>
+#include <vector>
+
+
+using namespace std;
+
+#define LASTNUMBER 1000000000
+
+char* isPrime = new char[LASTNUMBER + 1];
+
+int primes = 0;
+
+int erato()
+{
+    omp_set_num_threads(omp_get_max_threads());
+    int lastNumberSqrt = (int)sqrt((double)LASTNUMBER);
+
+    //inicjalizacja
+#pragma omp parallel for
+    for (int i = 0; i <= LASTNUMBER; i++)
+    {
+        isPrime[i] = 1;
+    }
+
+    // szukanie liczb będocych nie pierwszymi
+#pragma omp parallel for schedule(dynamic)
+    for (int i = 2; i <= lastNumberSqrt; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= LASTNUMBER; j += i)
+            {
+                isPrime[j] = 0;
+            }
+        }
+    }
+
+    if (LASTNUMBER >= 2)
+    {
+        primes = 1;
+    }
+
+    // liczenie ilości liczb pierwszych
+#pragma omp parallel for reduction(+:primes)
+    for (int i = 2; i <= LASTNUMBER; i++)
+    {
+        primes += isPrime[i];
+    }
+
+    return primes;
+}
+
+int main()
+{
+    int primesFound;
+
+    auto start = chrono::steady_clock::now();
+    primesFound = erato();
+    auto end = chrono::steady_clock::now();
+
+    cout << "znaleziono: " << primesFound << " liczb pierwszych" << endl;
+
+    cout << "Time in milliseconds: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+    cout << "Time in seconds: " << chrono::duration_cast<chrono::seconds>(end - start).count() << endl;
+}
+
