244ac70f863d377189a79db3931dde89f94595c7
Adam <252741@student.pwr.edu.pl>
2022-12-05 07:17:28 +0100

    More small changes

--- GPU/MeasureTime.py
+++ GPU/MeasureTime.py
@@ -1,23 +1,22 @@
 import numpy as np
 import cupy as cp
 from cupyx.profiler import benchmark
+import cupyx.profiler as profiler
 
 from SitoGPU import sito
 
 def main():
-    print("Input N:")
-    N = int(input())
+    N = 100000
     data_array = cp.asarray(np.ones(N+1, dtype=int))
     data_array[0] = 0
     data_array[1] = 0
     kernel = sito()
-    print(benchmark(kernel, (((N+1+255)//256,), (256,),(data_array, N+1))))
+    with profiler.profile():
+        print(benchmark(kernel, (((N+1+255)//1024,), (1024,),(data_array, N+1)), n_repeat=10))
     """
      for index, elem in enumerate(cp.asnumpy(data_array)):
             if elem:
             print(index, end= ", ")
     """
-    return
-
 if __name__ == "__main__":
     main()
